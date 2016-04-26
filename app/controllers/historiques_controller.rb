c65c90de36b41bf803ee68bd8f557974cc20bcea
ROPERTMorgane <ropert.morgane@gmail.com>
2016-04-26 10:27:43 +0200

    marge 3 branch

--- app/controllers/historiques_controller.rb
+++ app/controllers/historiques_controller.rb
@@ -1,2 +1,35 @@
-class HistoriquesController < ApplicationController
+class HistoriqueController < ApplicationController
+	before_action :set_duree_max, only: [:new, :create]
+  before_action :maj_listeattente, only: [:create]
+
+	def new
+	  @historique = Historique.new
+	end
+
+	def create
+	  @historique = Historique.new(historique_params)
+      datemax = Date.today + @dureemax.months
+
+	  respond_to do |format|
+      if params[:date_debut] >= Date.today && params[:date_fin] <= datemax && params[:date_debut] <= params[:date_fin]
+        if @historique.save
+          format.html { redirect_to index_utilisateur_path, notice: 'Demande enregistée.' }
+        else
+          format.html { render :new, notice: 'Donnée invalide.' }
+        end
+      else
+        format.html { render :new, notice: 'Donnée invalide.' }
+      end
+    end
+	end
+
+	private
+
+	def set_duree_max
+	  @dureemax = Parking.duration.maxduration
+	end
+
+	def historique_params
+      params.require(:historique).permit(:utilisateur_id, :placeparking_id, :date_debut, :date_fin)
+	end
 end
--- app/controllers/utilisateurs_controller.rb
+++ app/controllers/utilisateurs_controller.rb
@@ -1,24 +1,20 @@
-class UtilisateursController < ApplicationController
+class UtilisateurController < ApplicationController
   before_action :set_utilisateur, only: [:edit, :update, :index]
   before_action :authorization, only: [:edit, :update]
   before_action :uselessaction, only: [:new, :create]
   
-  def index 
-	  @historique = Historique.where(utilisateurs_id: current_utilisateur)
-	  @listeattente = Listeattente.where(utilisateurs_id: current_utilisateur)
-	  if @historique
+  def index
+    @historique = Historique.where(utilisateur_id: current_utilisateur)
+    @listeattente = Listeattente.find_by(utilisateur_id: current_utilisateur)
+    
+    @choix = 0
+    if @listeattente
+      @choix = 1
+    elsif @historique
       @historique.each do |h|
-	      if h.date_fin <= Date.today
-          @choix = 1
-	      elsif h.date_fin > Date.today && @listeattente.nil?
-	  	    @choix = 2
-	      elsif h.date_fin > Date.today && @listeattente
-	  	    @choix = 3
-	      else
-          @choix = 0
-	  	  end
-	    end
-	  end
+        @choix = 2 if Date.today >= h.date_fin && Date.today <= h.date_debut
+      end
+    end
   end
 
   def new
@@ -26,16 +22,12 @@ class UtilisateursController < ApplicationController
   end
 
   def create
-	  @utilisateur = Utilisateur.new(user_params_create)
-    @utilisateur.admin = true
-    @utilisateur.compte_accepted = true
+	@utilisateur = Utilisateur.new(utilisateur_params_create)
     respond_to do |format|
       if @utilisateur.save
-        format.html { redirect_to root_path, notice: 'Nouveau compte créé.' }
-        format.json { redirect_to root_path, status: 302, location: @utilisateur }
+        format.html { redirect_to index_utilisateurs_path, notice: 'Nouveau compte créé.' }
       else
-        format.html { render :new, notice: 'Donnée invalide.' }
-        format.json { render json: @utilisateur.errors, status: 422 }
+        format.html { render :new, notice: 'Données invalide.' }
       end
     end
   end
@@ -47,12 +39,10 @@ class UtilisateursController < ApplicationController
     respond_to do |format|
       if @utilisateur.nil?
         fail ActiveRecord::RecordNotFound
-      elsif @utilisateur.update(user_params_update)
+      elsif @utilisateur.update(utilisateur_params_update)
         format.html { redirect_to @utilisateur, notice: 'Compte mis à jour.' }
-        format.json { render :show, status: 200, location: @utilisateur }
       else
         format.html { render :edit }
-        format.json { render json: @utilisateur.errors, status: 422 }
       end
     end
   end
@@ -63,15 +53,14 @@ class UtilisateursController < ApplicationController
     render action: 'utilisateur_missing', status: 404
   end
 
-  def user_params_create
-    params.require(:utilisateur).permit(:username, :nom, :prenom, :tel, :mail, 
-                                        :password, :password_confirmation, :ligue_id)
+  def utilisateur_params_create
+    params.require(:utilisateur).permit(:username, :mail, :password, :password_confirmation,
+    	                                :nom, :prenom, :tel, :ligue_id)
   end
 
-  def user_params_update
-    params.require(:utilisateur).permit(:nom, :prenom, :tel,
-                                        :password, :password_confirmation, 
-                                        :adresse, :cp, :ville)
+  def utilisateur_params_update
+    params.require(:utilisateur).permit(:password, :password_confirmation,
+    	                                :nom, :prenom, :adresse, :cp, :ville, :tel)
   end
 
   def set_utilisateur
@@ -80,7 +69,7 @@ class UtilisateursController < ApplicationController
   end
 
   def authorization
-    redirect_to root_path, notice: "Vous ne pouvez pas faire cela." unless current_utilisateur.id == @utilisateur.id
+    redirect_to index_utilisateur_path, notice: "Vous ne pouvez pas faire cela." unless current_utilisateur.id == @utilisateur.id
   end
 
   def uselessaction
